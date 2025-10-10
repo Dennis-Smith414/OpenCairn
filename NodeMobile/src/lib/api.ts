2d2ed5c9d216e8b3111d6cf71854cf0619f22fda
Felix Koch <168679135+fkoch03@users.noreply.github.com>
2025-10-10 11:50:02 -0500

    Got it to work!

--- NodeMobile/src/lib/api.ts
+++ NodeMobile/src/lib/api.ts
@@ -1,5 +1,5 @@
 // src/lib/api.ts
-export const API_BASE = 'http://172.18.96.1:5100';
+export const API_BASE = 'http://10.0.2.2:5100';
 
 export async function fetchRouteList() {
   const res = await fetch(`${API_BASE}/api/routes/list`);
